38aedd93bfa98c9a387903fb49c21755ad899228
Glenn Law <Glennlaw@me.com>
2020-05-23 07:21:37 -0400

    adding Wanderlust project

---
+++ Work Around module/workAround.js
@@ -0,0 +1,12 @@
+import Employee from './employee';
+
+function getEmployeeInformation(inputSalary) {
+  Employee.salary = inputSalary;
+  console.log('Cadre: ' + Employee.getCadre());
+  console.log('Tax: ' + Employee.calculateTax());
+  console.log('Benefits: ' + Employee.getBenefits());
+  console.log('Bonus: ' + Employee.calculateBonus());
+  console.log(
+    'Reimbursement Eligibility: ' + Employee.reimbursementEligibility() + '\n'
+  );
+}
