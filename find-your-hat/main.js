390fd40b3ee28deb609171e17b733b6e22f81076
Glenn Law <Glennlaw@me.com>
2020-06-08 18:14:37 -0400

    Added excercise Javascript Drum set. This exercise hepled me query and manipulate the DOM

---
+++ find-your-hat/main.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
